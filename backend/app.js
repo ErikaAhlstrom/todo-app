adb03ee522809932a69ea67cbb618934112a5fd3
“erikaahlstrom” <info@erikasoldh.se>
2021-10-01 15:47:19 +0200

    create add user route.

--- backend/app.js
+++ backend/app.js
@@ -12,6 +12,7 @@ require('dotenv').config();
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const listsRouter = require('./routes/lists');
 
 const app = express();
 const port = process.env.PORT || 5000
@@ -33,6 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/lists', listsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,41 @@
-var express = require('express');
-var router = express.Router();
-
+const express = require('express');
+const router = express.Router();
+let User = require('../models/user.model')
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+/* router.get('/', function(req, res, next) {
+  res.send('Hey from Users Router');
+}); */
 
+
+// Get all users
+router.route('/')
+  .get((req, res, next) => {
+    User.find()
+      .then(users => res.json(users))
+      .catch(err => res.status(400).json('Error: ' + err))
+  })
+
+// Add a user
+router.route('/add')
+  .post((req, res, next) => {
+   
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password
+    });
+    
+   
+
+    newUser.save()
+      .then(() => res.json('New user added!'))
+      .catch(err => res.status(400).json('Error: ' + err)); 
+
+  })
 module.exports = router;
