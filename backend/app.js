1d5d57ea0dc9e762e1293c27fa10d55b4ffad787
“erikaahlstrom” <info@erikasoldh.se>
2021-10-01 13:49:35 +0200

    Set up port and server.

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,30 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// vad gör denna??
+const cors = require('cors');
 
-var app = express();
+require('dotenv').config();
+
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+const port = process.env.PORT || 5000
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
+
+// vad gör denna??
+app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -37,5 +48,9 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`)
+})
 
 module.exports = app;
