d22998ac55218d6c85a529493e247bf7e918f6f5
“erikaahlstrom” <info@erikasoldh.se>
2021-10-05 16:53:30 +0200

    Add line under list title.

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -7,20 +7,18 @@ export default function ListItem({list}) {
     return (
         <div className="list">
           <div className="list-header">
-            <h4>{list.title}</h4>
+            <h3>{list.title}</h3>
             <div className="list-action-btns">
               <Link to='/list/edit' className= "list-edit-btn" href="#"><i className='bx bx-edit-alt'></i></Link>
               <Link to='/list/delete' className= "list-delete-btn" href="#"><i className='bx bx-trash'></i></Link>
             </div>
           </div>
-          <div className="todo is-complete">
-            <div className="checkbox"></div>
+          <hr className="list-line"></hr>
             <div className="text">
                 {list.todos.map(todo => (
                     <p>{todo}</p>
                 ))}
             </div>
-          </div>
           <p className="list-date">{
             <Moment format="YYYY-MM-DD">{list.updatedAt}</Moment>}
           </p>
