cdf3f6cf31caa9c787b900047bddc3fe66345e49
“erikaahlstrom” <info@erikasoldh.se>
2021-10-24 15:38:44 +0200

    Create separate fetch file containing all fetches.

---
+++ src/fetches/fetches.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+// Middleware = Allows HTTP to set cookies
+axios.defaults.withCredentials = true;
+
+const url = axios.create({ baseURL: 'http://localhost:5000/' });
+
+
+
+/////// USER ////////
+export const editUser = (payload) => url.post('users/update', payload);
+export const getLoggedInFetch = () => url.get('auth/loggedIn');
+export const logoutUserFetch = () => url.get('auth/logout');
+export const loginUserFetch = (payload) => url.post('auth/login', payload);
+export const registerUserFetch = (payload) => url.post('auth', payload);
+
+
+/////// LIST ////////
+export const getSingleListFetch = (listId) => url.get(`lists/${listId}`);
+export const editSingleListFetch = (listId, payload) => url.post(`lists/update/${listId}`, payload);
+export const deleteListFetch = (listId) => url.delete(`lists/${listId}`);
+export const getListsForOneUserFetch = () => url.get('lists');
+
