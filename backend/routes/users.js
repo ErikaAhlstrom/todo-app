b5a1a5cf32b300a56ef2aa97f6a39c37e976881f
“erikaahlstrom” <info@erikasoldh.se>
2021-10-24 16:19:27 +0200

    Clean up code and add getOneUser route.

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,89 +3,86 @@ const router = express.Router();
 let User = require('../models/user.model')
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken')
+const auth = require("../middleware/auth")
+const checkUser = require('../utils/checkUser');
 
 
-// Get all users
-router.route('/')
-  .get((req, res, next) => {
-    User.find()
-      .then(users => res.json(users))
-      .catch(err => res.status(400).json('Error: ' + err))
-  })
 // Register a user
-  .post( async (req, res, next) => {
-    try {
-      const {firstName, lastName, email, password } = req.body
+router.post('/register', async (req, res, next) => {
+  try {
+    const {firstName, lastName, email, password } = req.body
 
-      if(!email || !password) {
-        return res.status(400).json({errorMessage: "Please fill in all required fields"})
-      }
-
-      if(password.length < 6) {
-        return res.status(400).json({errorMessage: "Please enter a password with at least 6 characters."})
-      }
-
-      const existingUser = await User.findOne({email})
-      if(existingUser) {
-        return res.status(400).json({
-          errorMessage: "This email already exists"
-        })
-      }
-
-      // Hash the password
-
-      const salt = await bcrypt.genSalt();
-      const passwordHash = await bcrypt.hash(password, salt)
-
-      // Save user account to the db
-
-      const newUser = new User({
-        "firstName": firstName,
-        "lastName": lastName,
-        "email": email,
-        "password": passwordHash
-      })
-
-      const savedUser = await newUser.save();
-      console.log(newUser)
-
-      // sign the token
-
-      const token = jwt.sign(
-        {
-        user: savedUser._id
-        }, 
-      process.env.JWT_SECRET
-      );
-
-      console.log(token);
-
-      // send the token in a HTTP only cookie
-      // När cookien är set kommer den nu alltid skickas med till servern för att verifiera att personen är inloggad. Om jag fattat rätt?
-      res
-        .cookie("token", token, {
-          httpOnly: true
-        })
-        .send();
-
-    } catch (err) {
-      console.error(err);
-      res.status(500).send();
+    if(!email || !password) {
+      return res.status(400).json({errorMessage: "Please fill in all required fields"})
     }
 
-  })
+    if(password.length < 6) {
+      return res.status(400).json({errorMessage: "Please enter a password with at least 6 characters."})
+    }
 
-/* // Get one user
-router.route('/:id')
-  .get((req, res, next) => {
-    User.findById(req.params.id)
-      .then(user => res.json(user))
-      .catch(err => res.status(400).json('Error: ' + err))
-  })
- */
+    const existingUser = await User.findOne({email})
+    if(existingUser) {
+      return res.status(400).json({
+        errorMessage: "This email already exists"
+      })
+    }
+
+    // Hash the password
+
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt)
+
+    // Save user account to the db
+
+    const newUser = new User({
+      "firstName": firstName,
+      "lastName": lastName,
+      "email": email,
+      "password": passwordHash
+    })
+
+    const savedUser = await newUser.save();
+    console.log(newUser)
+
+    // sign the token
+
+    const token = jwt.sign(
+      {
+      user: savedUser._id
+      }, 
+    process.env.JWT_SECRET
+    );
+
+    console.log(token);
+
+    // send the token in a HTTP only cookie
+    res
+      .cookie("token", token, {
+        httpOnly: true
+      })
+      .send();
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).send();
+  }
+
+})
+
+// Get one user
+router.get('/current', auth, async(req, res, next) => {
+  try {
+    const userId = checkUser(req.cookies.token)
+    const user = await User.findById(userId)
+    res.json(user)   
+  } catch (err) {
+      console.error(err);
+      res.status(500).send()
+    }
+})
 
 // Log in a user
-router.post("/login", async (req, res) => {
+router.post("/login", async (req, res, next) => {
   try {
     const { email, password } = req.body;
 
@@ -140,15 +137,13 @@ router.post("/login", async (req, res) => {
   }
 });
 
-
-router.route('/logout')
-  .get((req, res) => {
+router.get('/logout', async(req, res, next) => {
     res.cookie("token", "", {
       httpOnly: true,
       expires: new Date(0),
     })
   .send();
-  })
+})
 
 router.get("/loggedIn", (req, res) => {
    try {
