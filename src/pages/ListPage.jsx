ded6a0dec09285f50996d355d9bdbfe50d6c8017
“erikaahlstrom” <info@erikasoldh.se>
2021-10-25 18:19:40 +0200

    Add message on homepage if no lists.

--- src/pages/ListPage.jsx
+++ src/pages/ListPage.jsx
@@ -12,19 +12,27 @@ export default function ListPage({lists, user}) {
         <div className="listPage">
             <h1 className="header-1"> Welcome, {user.firstName}</h1>
             <div className="lists-header">
+                {lists.length > 0 
+                ? 
+
                 <h2 className="header-2">Your Notes</h2>
+                : 
+               <h2 className="header-2">No notes to show yet...</h2>}
                 <div>
                 <LogoutBtn />
                 <Link to='/list/create' className="btn new-note-btn btn-success">New Note</Link>
                 </div>
                 
             </div>   
-
-            <div className="lists-container">
-                {lists.map(list => (
-                        <ListItem key={list._id} list={list}/>
-                ))}
-            </div>
+                {lists.length === 0 ?
+                <button className="btn btn-primary">Create your first note</button>
+                :
+                <div className="lists-container">
+                    {lists.map(list => (
+                            <ListItem key={list._id} list={list}/>
+                    ))}
+                </div>
+                }
         </div>
     )
 }
