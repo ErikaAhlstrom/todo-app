4687740b2236d76401cc7adf54bedaa888e1ad20
“erikaahlstrom” <info@erikasoldh.se>
2021-11-02 20:04:37 +0100

    Create validation file in utils folder.

---
+++ backend/utils/validationUser.js
@@ -0,0 +1,18 @@
+
+const checkEmptyFieldsRegister = (email, password, firstName, lastName) => {
+    if(!email || !password || !firstName || !lastName) return false
+    return true
+}
+
+const checkEmptyFieldsLogin = (email, password) => {
+    if(!email || !password ) return false
+    return true
+}
+const checkPasswordLength = (password) => {
+    if(password.length < 6) return false
+    return true
+}
+
+module.exports = {checkEmptyFieldsRegister, checkEmptyFieldsLogin, checkPasswordLength};
+    
+    
