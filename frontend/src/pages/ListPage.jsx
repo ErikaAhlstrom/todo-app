63af8cc08f2d4b61e2f0259ee8eb8a69357f3c41
erikaahlstrom <info@erikasoldh.se>
2022-02-19 12:32:31 +0100

    Change layout on list page.

--- frontend/src/pages/ListPage.jsx
+++ frontend/src/pages/ListPage.jsx
@@ -10,23 +10,21 @@ export default function ListPage({lists, user}) {
 
     return (
         <div className="listPage">
-            <h1 className="header-1"> Welcome, {user.firstName}</h1>
+            <h1 className="header-1"> Welcome {user.firstName}</h1>
+                <div className='header-buttons'>
+                <Link to='/list/create' className="btn new-note-btn btn-success">New Note</Link>
+                <LogoutBtn />
+                </div>
             <div className="lists-header">
                 {lists.length > 0 
                 ? 
                 <h2 className="header-2">Your Notes</h2>
                 : 
                <h2 className="header-2">You haven't got any notes yet</h2>}
-                <div>
-                <LogoutBtn />
-                <Link to='/list/create' className="btn new-note-btn btn-success">New Note</Link>
-                </div>
                 
             </div>   
-                {lists.length === 0 ?
-                <a href="/list/create" className="btn btn-primary">Create your first note</a>
-                :
-                <div className="lists-container">
+                {lists.length !== 0 &&
+                 <div className="lists-container">
                     {lists.map(list => (
                         <ListItem key={list._id} list={list}/>
                     ))}
